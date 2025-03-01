c32ff77a5daaca8f1b74d863c7334f52d387dcef
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-03-01 15:36:37 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 // import NewArticle from "./pages/NewArticle";
-import Homepage from "./pages/Homepage";
+// import Homepage from "./pages/Homepage";
 // import HomePage2 from "./Component/Homepage2";
 import Usa from "./New-speech/Usa";
 import China from "./New-speech/China";
@@ -13,13 +13,14 @@ import Nigeria from "./New-speech/Nigeria";
 import Ukraine from "./New-speech/Ukraine";
 import Japan from "./New-speech/Japan";
 import Argentina from "./New-speech/Argentina";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <Routes>
       {/* <Route path="/" element={<Homepage />} /> */}
 
-      <Route path="/Homes" element={<Homepage />} />
+      <Route path="/" element={<Homepage />} />
       <Route path="/Usa-News" element={<Usa />} />
       <Route path="/China-News" element={<China />} />
       <Route path="/India-News" element={<India />} />
