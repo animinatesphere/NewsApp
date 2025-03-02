4519f8c057432e2f7291818b0a69df2b4145d467
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-03-02 15:39:40 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import "./App.css";
 // import NewArticle from "./pages/NewArticle";
 // import Homepage from "./pages/Homepage";
 // import HomePage2 from "./Component/Homepage2";
-import Usa from "./New-speech/Usa";
-import China from "./New-speech/China";
+import Usa from "./new-speech/Usa";
+import China from "./new-speech/China";
 import { Apple } from "./pages/Apple";
 import Tesla from "./Component/Tesla";
 import TechCrunch from "./Component/TechCrunch";
-import India from "./New-speech/India";
-import Nigeria from "./New-speech/Nigeria";
-import Ukraine from "./New-speech/Ukraine";
-import Japan from "./New-speech/Japan";
-import Argentina from "./New-speech/Argentina";
+import India from "./new-speech/India";
+import Nigeria from "./new-speech/Nigeria";
+import Ukraine from "./new-speech/Ukraine";
+import Japan from "./new-speech/Japan";
+import Argentina from "./new-speech/Argentina";
 import Homepage from "./pages/Homepage";
 
 function App() {
